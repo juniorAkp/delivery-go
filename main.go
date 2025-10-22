b73d3c98d8e8f05edb38d2f9eba4cac7034935ec
Niiaks <napappoe007@st.ug.edu.gh>
2025-10-22 20:00:19 +0000

    graceful server shutdown

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,6 +22,13 @@ func main() {
 		port = "8080"
 	}
 
+	var client *mongo.Client = database.Connect()
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		log.Fatal("Failed to connect to mongo db")
+	}
+	log.Println("mongoDb connected")
+
 	r := NewRouter()
 
 	srv := &http.Server{
@@ -29,24 +38,40 @@ func main() {
 		ReadHeaderTimeout: 5 * time.Second,
 	}
 
-	if err := srv.ListenAndServe(); err != nil {
-		log.Printf("Failed to start server: %v", err)
+	//start server in background
+	go func() {
+		log.Printf("server is listening on %s", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("Failed to start server: %v", err)
+		}
+	}()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+	<-stopChan
+
+	log.Println("initializing graceful shutdown")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("Http server shutdown error: %v", err)
+	} else {
+		log.Println("Http server shutdown")
 	}
+
+	if err := client.Disconnect(shutdownCtx); err != nil {
+		log.Printf("MongoDB disconnect error: %v", err)
+	} else {
+		log.Println("MongoDB disconnected")
+	}
+
+	log.Println("Shutdown complete")
 }
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
-	var client *mongo.Client = database.Connect()
-
-	if err := client.Ping(context.Background(), nil); err != nil {
-		log.Fatal("Failed to connect to mongo db")
-	}
-
-	defer func() {
-		if err := client.Disconnect(context.Background()); err != nil {
-			panic(err)
-		}
-	}()
 
 	r.Use(middleware.Cors())
 
