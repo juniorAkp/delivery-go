f71f75cab39ef608e70b0747f40f6d03d280063f
Niiaks <napappoe007@st.ug.edu.gh>
2025-10-24 10:27:12 +0000

    add protected middleware

--- routes/protected.go
+++ routes/protected.go
@@ -3,11 +3,12 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/juniorAkp/delivery-go/controller"
+	"github.com/juniorAkp/delivery-go/middleware"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func ProtectedRoute(router *gin.RouterGroup, client *mongo.Client) {
-
+	router.Use(middleware.AuthRequired())
 	router.POST("/orders", controller.CreateOrder(client))
 
 }
