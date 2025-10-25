8c372f04d7e433bd3b6eccc94ac15a0ecf62d42f
Niiaks <napappoe007@st.ug.edu.gh>
2025-10-25 21:51:04 +0000

    setup types for ws connection

--- utils/types.go
+++ utils/types.go
@@ -1,6 +1,38 @@
 package utils
 
+import (
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
 type Coord struct {
 	Longitude float64 `json:"longitude" validate:"required"`
 	Latitude  float64 `json:"latitude" validate:"required"`
+}
+
+type ConnectionHub struct {
+	activeCustomers map[string]*UserConnection
+	activeCouriers  map[string]*UserConnection
+	messageQueue    chan *OutgoingMessage
+	registerConn    chan *UserConnection
+	removeConn      chan *UserConnection
+	mutex           sync.RWMutex
+}
+
+type UserConnection struct {
+	UserID          string
+	AccountType     string
+	Socket          *websocket.Conn
+	OutgoingMessage chan []byte
+	Manager         *ConnectionHub
+}
+
+type OutgoingMessage struct {
+	MessageType string    `json:"type"`
+	SenderID    string    `json:"from"`
+	ReceiverID  string    `json:"to,omitempty"`
+	Content     any       `json:"data"`
+	SentAt      time.Time `json:"timeStamp"`
 }
